e2cb39d24095ce0b0162cd3e274a3a5594f56d6e
Leslieluvutse <flexingleslie@gmail.com>
2024-07-08 22:49:52 +0300

    created the sign up form on the signupviewvue

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,5 +1,32 @@
+<script setup>
+   import {ref} from 'vue'
+
+   const Username =ref('john')
+   const email = ref('john@gmail.com')
+   const password = ref('1233')
+
+   function signup(){
+              console.log(Username.value,email.value,password.value)
+   }
+ </script>
 <template>
               <div>
-                            Signup Form
+                           <h1>Signup Form</h1>
+                          <form @submit="signup">
+                            <div>
+                                          <input type="text" placeholder="Enter Username" v-model="Username">
+                            </div>
+                            <div>
+                                          <input type="email" placeholder="Enter Email" v-model
+                                          ="email">
+                            </div>
+                            <div>
+                                          <input type="password" placeholder="Enter Password" v-model="password">
+                            </div>
+              <div>
+                            
+                            <input type="Sign-Up" placeholder="Signup" v-model="signup">
+              </div>
+                          </form> 
               </div>
 </template>
